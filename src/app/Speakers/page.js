75bbcc64efc0a4fb9b39a5e5a0c31bec361f6d50
arnava-vash2612 <av.vash2612@gmail.com>
2026-05-07 22:43:44 +0530

    Fix Swiper v12 CSS imports on Speakers page

--- src/app/Speakers/page.js
+++ src/app/Speakers/page.js
@@ -6,9 +6,7 @@ import Navbar from "../home/Navbar";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
 
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
+import "swiper/swiper-bundle.css";
 
 const SpeakersSection = () => {
   const speakers = [
